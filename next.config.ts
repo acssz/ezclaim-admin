dd5f794c89294e0e97b50d22cacb805e2b175957
Karl Liu <karl.liu.1024@gmail.com>
2025-10-10 05:25:59 +0200

    deploy: add gcs to next.js image whitelist

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,15 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '2mb'
     }
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ezclaim.storage.googleapis.com',
+        pathname: '/**',
+      }
+    ],
+  },
   output: 'standalone',
 };
 
